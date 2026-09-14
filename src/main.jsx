bc2977bb0bc760737ddb11dbde33bfd4e5b7540a
ADIYAN <adiyanhehe@gmail.com>
2026-09-14 17:48:46 +0700

    Load enhanced feature card and example styling

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './animations.css';
 import './feature-enhancement.css';
+import './feature-upgrade.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
